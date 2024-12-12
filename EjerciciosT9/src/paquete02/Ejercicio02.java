a417b5e9b34cc13e0b020caaa89f111df1144b4f
Javier Vinan <javinan5@utpl.edu.ec>
2024-12-12 12:23:44 -0500

    problema02

--- EjerciciosT9/src/paquete02/Ejercicio02.java
+++ EjerciciosT9/src/paquete02/Ejercicio02.java
@@ -16,7 +16,7 @@ public class Ejercicio02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] respuestas = {1, 10, 11, 12, 12, 13, 16, 17, 18, 8, 9, 10, 21, 
+        int[] respuestas = {1, 10, 11, 12, 12, 13, 16, 17, 18, 8, 9, 10, 21,
             -1, 100, 200, 2, 21, 22, 27};
         String[] rangos = {"1-5", "6-10", "11-15", "16-20"};
         int[] valoresRangos = new int[4]; // {0, 0, 0, 0, 0}
@@ -24,23 +24,19 @@ public class Ejercicio02 {
 
         for (int i = 0; i < respuestas.length; i++) {
             respuesta = respuestas[i]; // -1
-            if ((respuesta >= 1) && (respuesta <= 5)) {
+            if ((respuesta < 1) || (respuesta > 5)) {
                 valoresRangos[0] = valoresRangos[0] + 1;
-            } else {
-                if ((respuesta >= 6) && (respuesta <= 10)) {
-                    valoresRangos[1] = valoresRangos[1] + 1;
-                } else {
-                    if ((respuesta >= 11) && (respuesta <= 15)) {
-                        valoresRangos[2] = valoresRangos[2] + 1;
-                    } else {
-                        if ((respuesta >= 16) && (respuesta <= 20)) {
-                            valoresRangos[3] = valoresRangos[3] + 1;
-                        }
-
-                    }
-                }
-
             }
+            if ((respuesta < 6) || (respuesta > 10)) {
+                valoresRangos[1] = valoresRangos[1] + 1;
+            }
+            if ((respuesta < 11) || (respuesta > 15)) {
+                valoresRangos[2] = valoresRangos[2] + 1;
+            }
+            if ((respuesta < 16) || (respuesta > 20)) {
+                valoresRangos[3] = valoresRangos[3] + 1;
+            }
+
         }
 
         for (int i = 0; i < rangos.length; i++) {
