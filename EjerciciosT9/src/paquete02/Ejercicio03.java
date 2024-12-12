5c21e25c93cf4845a1b2d458e9b954799039a6c4
Javier Vinan <javinan5@utpl.edu.ec>
2024-12-12 12:32:14 -0500

    problema03

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -17,7 +17,28 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        
+        int[] media = new int [2];
+        double suma = 0;
+        double promedio = 0;
+        for(int i = 0; i < arreglo.length; i++){
+            suma = suma + arreglo[i];
+            
+        }
+        promedio = suma/arreglo.length;
+        for(int i = 0; i < arreglo.length; i++){
+            if(arreglo[i] > promedio){
+                media[0] = media[0] + 1;
+            }else{
+                media[1] = media[1] + 1; 
+            }
+            
+        }
+        System.out.printf("La media aritmetica es: %.2f\n"
+                + "Numeros mayores a la media: %d\n"
+                + "Numeros menores a la media: %d\n",
+                promedio,
+                media[0],
+                media[1]);
     }
 
 }
